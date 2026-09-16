14c2fe5d9bc01375460ace33cf95ab2ba0fc4cdd
Deb-das <swastidevdas@gmail.com>
2026-09-17 00:26:09 +0530

    done

--- main.cpp
+++ main.cpp
@@ -7,26 +7,77 @@ int main()
     string line;
     string ans = "";
 
-    while(getline(cin, line))
+    getline(cin, line);
+
+    int pos = line.find(' ');
+    int statusCode = stoi(line.substr(0, pos));
+    int headerCount = stoi(line.substr(pos + 1));
+
+    ans += "HTTP/1.1 ";
+
+    if(statusCode == 200)
+        ans += "200 OK\r\n";
+    else if(statusCode == 201)
+        ans += "201 Created\r\n";
+    else if(statusCode == 204)
+        ans += "204 No Content\r\n";
+    else if(statusCode == 400)
+        ans += "400 Bad Request\r\n";
+    else if(statusCode == 404)
+        ans += "404 Not Found\r\n";
+    else if(statusCode == 401)
+        ans += "401 Unauthorized\r\n";
+    else if(statusCode == 500)
+        ans += "500 Internal Server Error\r\n";
+    else if(statusCode == 301)
+        ans += "301 Moved Permanently\r\n";
+
+    bool hasContentLength = false;
+
+    for(int i = 0; i < headerCount; i++)
     {
-        if(line.empty())
-            continue;
+        getline(cin, line);
 
-        int num = stoi(line, nullptr, 16);
+        if(!line.empty() && line.back() == '\r')
+            line.pop_back();
 
-        if(num == 0)
-            break;
+        ans += line + "\r\n";
 
-        string data;
-        getline(cin, data);
+        int colon = line.find(':');
 
-        for(int i = 0; i < num && i < data.size(); i++)
+        string name = line.substr(0, colon);
+
+        if(name == "Content-Length" ||
+           name == "content-length")
         {
-            ans += data[i];
+            hasContentLength = true;
         }
     }
 
-    cout << ans << endl;
+    string body = "";
+
+    while(getline(cin, line))
+    {
+        if(!line.empty() && line.back() == '\r')
+            line.pop_back();
+
+        if(!body.empty())
+            body += "\n";
+
+        body += line;
+    }
+
+    if(!hasContentLength)
+    {
+        ans += "Content-Length: ";
+        ans += to_string(body.size());
+        ans += "\r\n";
+    }
+
+    ans += "\r\n";
+    ans += body;
+
+    cout << ans;
 
     return 0;
 }
